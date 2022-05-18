b014403af78d25555affdea90265e7dd496c8b22
timhedspeth <71342657+timhedspeth@users.noreply.github.com>
2022-05-18 09:11:43 -0400

    Update thesdpet_visual.jl

--- thesdpet_visual.jl
+++ thesdpet_visual.jl
@@ -3,19 +3,27 @@
 # This script must be copied into the Julia in terminal for it to work 
 
 # Load in the packages 
-using DataFrames, CSV, Plots, PlotlyJS
+using DataFrames
+using CSV
+using Plots
+using PlotlyJS
 
-# Define the file we are using 
-input_file = open("/gpfs/data/biol1555/projects2022/smoking.txt", "r")
+# Read in the data frame 
+input_file = open("/gpfs/data/biol1555/thedspet/1_code/vitalcleaned.txt", "r")
 
-# Create the boxplot for age at gestational death based on the mother's smoking status 
-    smoking_data_frame = CSV.File(input_file, header=1, footerskip=0, delim="|") |> DataFrame
+# Create the boxplots 
+function plot()
+    smokeplot = CSV.File(input_file, header=1, footerskip=0, delim="|") |> DataFrame
     plotlyjs()
-    display(PlotlyJS.plot(smoking_data_frame, x=:Smoke_Status, y=:Gestational_Age, kind="box", mode="markers",
-    Layout(title="Age at Death, based on Mother's Smoking Status",
-    xaxis_title= "Smoking Status of Mother", yaxis_title= "Gestastional Age")
+    display(PlotlyJS.plot(smokeplot, x=:Smoke_Status, y=:Gestational_Age, kind="box", mode="markers",
+    Layout(title="Gestational Age at Death Between Birth Locations",
+    xaxis_title= "Birth Setting", yaxis_title= "Gestastional Age (Weeks)")
     ))
-    display(PlotlyJS.plot(smoking_data_frame, x=:Smoke_Status, y=:Gestational_Age, kind="box", mode="markers",
-    Layout(title="Age at Death, based on Mother's Smoking Status",
-    xaxis_title= "Smoking Status of Mother", yaxis_title= "Gestastional Age")
+    display(PlotlyJS.plot(smokeplot, x=:Smoke_Status, y=:Gestational_Age, kind="box", mode="markers",
+    Layout(title="Gestational Age at Death Between General Birth Locations",
+    xaxis_title= "General Birth Setting", yaxis_title= "Gestastional Age (Weeks)")
     ))
+end
+
+# Run the function 
+plot()
